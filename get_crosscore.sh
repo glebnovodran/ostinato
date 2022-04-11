5c17332ac35efb1f78a58e212e4d05e110b1d163
Glib Novodran <novodran@gmail.com>
2022-04-11 12:28:00 +0200

    Fix get_crosscore.sh to replace the files fetched instead of adding as a numbered copy

--- get_crosscore.sh
+++ get_crosscore.sh
@@ -2,12 +2,12 @@
 if [ ! -d "crosscore" ]; then mkdir -p crosscore; fi
 for xcore in crosscore.hpp crosscore.cpp demo.hpp demo.cpp draw.hpp oglsys.hpp oglsys.cpp oglsys.inc scene.hpp scene.cpp smprig.hpp smprig.cpp draw_ogl.cpp
 do
-	wget -P crosscore https://raw.githubusercontent.com/schaban/crosscore_dev/main/src/$xcore
+	wget -O crosscore/$xcore https://raw.githubusercontent.com/schaban/crosscore_dev/main/src/$xcore
 done
 
 if [ ! -d "crosscore/ogl" ]; then mkdir -p crosscore/ogl; fi
 for xcore in gpu_defs.h progs.inc shaders.inc
 do
-	wget -P crosscore/ogl https://raw.githubusercontent.com/schaban/crosscore_dev/main/src/ogl/$xcore
+	wget -O crosscore/ogl/$xcore https://raw.githubusercontent.com/schaban/crosscore_dev/main/src/ogl/$xcore
 done
 
