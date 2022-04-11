8fa21c2e12d54bc22e4d607f6e247a238f4425a6
Glib Novodran <novodran@gmail.com>
2022-04-11 10:39:55 +0200

    build.sh to take additional params

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ if [ ! -d "./bin/linux" ]; then
 	mkdir -p ./bin/linux
 fi
 
-g++ -ggdb -ffast-math -ftree-vectorize -std=c++11 -I crosscore -I inc `ls src/*.cpp` `ls crosscore/*.cpp`  -o bin/linux/ostinato -ldl -lX11 -lpthread -DX11
+g++ -ggdb -ffast-math -ftree-vectorize -std=c++11 -I crosscore -I inc `ls src/*.cpp` `ls crosscore/*.cpp`  -o bin/linux/ostinato -ldl -lX11 -lpthread -DX11 $*
