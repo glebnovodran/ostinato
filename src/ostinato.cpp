ef8f42f55b692ee83c0fe47d657ce76ea31a68e9
Glib Novodran <novodran@gmail.com>
2022-04-17 15:22:44 +0200

    more -Wall build warnings cleanup

--- src/ostinato.cpp
+++ src/ostinato.cpp
@@ -26,7 +26,7 @@ static void oglsys_mem_free(void* pMem) {
 
 static void init_ogl(const int x, const int y, const int w, const int h, const int msaa) {
 	OGLSysCfg cfg;
-	::memset(reinterpret_cast<void*>(&cfg), 0, sizeof(cfg));
+	cfg.clear();
 	cfg.x = x;
 	cfg.y = y;
 	cfg.width = w;
