30cf939caccf141cedddc94cf610cff79fce7154
Glib Novodran <novodran@gmail.com>
2022-05-08 09:57:30 +0200

    Use nxCore::mem_zero instead of ::memset

--- src/ostinato.cpp
+++ src/ostinato.cpp
@@ -424,7 +424,7 @@ void update_sensors() {
 	if (fid >= 0) {
 		char inBuf[128];
 		char valBuf[128];
-		::memset(valBuf, 0, 128 * sizeof(char));
+		nxCore::mem_zero(valBuf, sizeof(valBuf));
 		size_t nread = ::read(fid, inBuf, sizeof(inBuf) - 1);
 		if (nread > 0) {
 			size_t valIdx = 0;
