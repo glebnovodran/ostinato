538e456979ffeb539c8d6418c8a9f43a48c8d27f
Glib Novodran <novodran@gmail.com>
2022-05-09 12:01:02 +0200

    opt.js : added 'low' mode

--- web/opt.js
+++ web/opt.js
@@ -12,6 +12,11 @@ if (pageParams.get("small") !== null) {
 	Module.arguments.push("-h:320");
 }
 
+if (pageParams.get("low") !== null) {
+	Module.arguments.push("-smap:1024");
+	Module.arguments.push("-nostgshadow:1");
+}
+
 if (pageParams.get("vl") !== null) {
 	Module.arguments.push("-vl:1");
 }
