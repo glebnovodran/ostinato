af9536fc8d8b1dbae49f040f8aa19439d12f5d44
Glib Novodran <novodran@gmail.com>
2022-05-17 14:22:15 +0200

    Move performance measurment code out of demo_default.cpp

--- src/demo_default.cpp
+++ src/demo_default.cpp
@@ -14,92 +14,14 @@
 #include "citizen.hpp"
 #include "player.hpp"
 #include "ostinato.hpp"
+#include "perfmon.hpp"
 
 DEMO_PROG_BEGIN
 
-enum MEASURE {
-	EXE = 0,
-	VISIBILITY,
-	DRAW,
-	NUM_TIMER
-};
 
 struct DemoWk {
-	struct {
-		cxStopWatch mAll[NUM_TIMER];
-		double mMedian[NUM_TIMER];
-
-		void init() {
-			for (int i = 0; i < NUM_TIMER; ++i) {
-				mAll[i].alloc(30);
-			}
-		}
-		void free() {
-			for (int i = 0; i < NUM_TIMER; ++i) {
-				mAll[i].free();
-			}
-		}
-		void begin(const MEASURE& m) {
-			mAll[m].begin();
-		}
-		void end(const MEASURE& m) {
-			if (mAll[m].end()) {
-				double us = mAll[m].median();
-				mAll[m].reset();
-				mMedian[m] = us / 1000.0;
-			}
-		}
-		double get_median(const MEASURE& m) const {
-			return mMedian[m];
-		}
-	} stopWatch;
-
-	struct {
-		double mSmps[30];
-		int mIdx;
-		uint32_t mTS0;
-		uint32_t mTS1;
-		double mMillis;
-		bool mFlg;
-
-		void init() {
-			mFlg = false;
-			nxCore::mem_zero(mSmps, sizeof(mSmps));
-			mIdx = 0;
-			mMillis = 0.0;
-			mTS0 = OGLSys::create_timestamp();
-			mTS1 = OGLSys::create_timestamp();
-		}
-
-		void begin() const { OGLSys::put_timestamp(mTS0); }
-		void end() const { OGLSys::put_timestamp(mTS1); }
-
-		void exec() {
-			if (mFlg) {
-				OGLSys::wait_timestamp(mTS1);
-				uint64_t t0 = OGLSys::get_timestamp(mTS0);
-				uint64_t t1 = OGLSys::get_timestamp(mTS1);
-				uint64_t dt = t1 - t0;
-				double millis = double(dt) * 1.0e-6;
-				int nsmp = XD_ARY_LEN(mSmps);
-				if (mIdx < nsmp) {
-					mSmps[mIdx] = millis;
-					++mIdx;
-				} else {
-					mIdx = 0;
-					mMillis = nxCalc::median(mSmps, nsmp);
-				}
-			}
-			mFlg = true;
-		}
-
-		void free() {
-			OGLSys::delete_timestamp(mTS0);
-			OGLSys::delete_timestamp(mTS1);
-			mTS0 = 0;
-			mTS1 = 0;
-		}
-	} perfGPU;
+	CPUPerfMon stopWatch;
+	GPUPerfMon perfGPU;
 
 	Pkg* pPkg;
 	sxCollisionData* pCol;
@@ -294,9 +216,11 @@ static void loop(void* pLoopCtx) {
 	Scene::exec();
 	view_exec();
 	s_demoWk.stopWatch.end(MEASURE::EXE);
+
 	s_demoWk.stopWatch.begin(MEASURE::VISIBILITY);
 	Scene::visibility();
 	s_demoWk.stopWatch.end(MEASURE::VISIBILITY);
+
 	s_demoWk.stopWatch.begin(MEASURE::DRAW);
 	s_demoWk.perfGPU.begin();
 	Scene::frame_begin(cxColor(0.5f));
@@ -304,6 +228,7 @@ static void loop(void* pLoopCtx) {
 	draw_2d();
 	s_demoWk.perfGPU.end();
 	s_demoWk.stopWatch.end(MEASURE::DRAW);
+
 	Scene::frame_end();
 }
 
---
+++ src/perfmon.hpp
@@ -0,0 +1,85 @@
+/* SPDX-License-Identifier: MIT */
+/* SPDX-FileCopyrightText: 2022 Glib Novodran <novodran@gmail.com> */
+
+enum MEASURE {
+	EXE = 0,
+	VISIBILITY,
+	DRAW,
+	NUM_TIMER
+};
+
+struct CPUPerfMon {
+	cxStopWatch mAll[NUM_TIMER];
+	double mMedian[NUM_TIMER];
+
+	void init() {
+		for (int i = 0; i < NUM_TIMER; ++i) {
+			mAll[i].alloc(30);
+		}
+	}
+	void free() {
+		for (int i = 0; i < NUM_TIMER; ++i) {
+			mAll[i].free();
+		}
+	}
+	void begin(const MEASURE& m) {
+		mAll[m].begin();
+	}
+	void end(const MEASURE& m) {
+		if (mAll[m].end()) {
+			double us = mAll[m].median();
+			mAll[m].reset();
+			mMedian[m] = us / 1000.0;
+		}
+	}
+	double get_median(const MEASURE& m) const {
+		return mMedian[m];
+	}
+};
+
+struct GPUPerfMon {
+	double mSmps[30];
+	int mIdx;
+	uint32_t mTS0;
+	uint32_t mTS1;
+	double mMillis;
+	bool mFlg;
+
+	void init() {
+		mFlg = false;
+		nxCore::mem_zero(mSmps, sizeof(mSmps));
+		mIdx = 0;
+		mMillis = 0.0;
+		mTS0 = OGLSys::create_timestamp();
+		mTS1 = OGLSys::create_timestamp();
+	}
+
+	void begin() const { OGLSys::put_timestamp(mTS0); }
+	void end() const { OGLSys::put_timestamp(mTS1); }
+
+	void exec() {
+		if (mFlg) {
+			OGLSys::wait_timestamp(mTS1);
+			uint64_t t0 = OGLSys::get_timestamp(mTS0);
+			uint64_t t1 = OGLSys::get_timestamp(mTS1);
+			uint64_t dt = t1 - t0;
+			double millis = double(dt) * 1.0e-6;
+			int nsmp = XD_ARY_LEN(mSmps);
+			if (mIdx < nsmp) {
+				mSmps[mIdx] = millis;
+				++mIdx;
+			} else {
+				mIdx = 0;
+				mMillis = nxCalc::median(mSmps, nsmp);
+			}
+		}
+		mFlg = true;
+	}
+
+	void free() {
+		OGLSys::delete_timestamp(mTS0);
+		OGLSys::delete_timestamp(mTS1);
+		mTS0 = 0;
+		mTS1 = 0;
+	}
+};
