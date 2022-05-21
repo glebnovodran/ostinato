f734c826e739b6585138b3fa1f8cb82a73ba02da
Glib Novodran <novodran@gmail.com>
2022-05-21 18:53:51 +0200

    unbundle.sh typo fix

--- util/bundle/unbundle.sh
+++ util/bundle/unbundle.sh
@@ -2,10 +2,10 @@
 
 EXE_DIR=$PWD
 BIN_DIR=$EXE_DIR/../../bin
-DAT_DIR=$BIN_DIR/dat
+DAT_DIR=$BIN_DIR/data
 TMP_DIR=$EXE_DIR/../../tmp
 
 OUT_DIR=$TMP_DIR/unbundled
 mkdir -p $OUT_DIR
 
-$TMP_DIR/bundle $BIN_DIR/ostinato.bnd -o:$OUT_DIR
+$TMP_DIR/bundle $DAT_DIR/ostinato.bnd -o:$OUT_DIR
