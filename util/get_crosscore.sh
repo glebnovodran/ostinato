02ea801ea7cc8a6183f45e42f02bc2c528a29899
Glib Novodran <novodran@gmail.com>
2022-05-24 17:24:47 +0200

    get_crosscore.sh : initial MacOS support

--- util/get_crosscore.sh
+++ util/get_crosscore.sh
@@ -1,16 +1,35 @@
 #!/bin/sh
+
 cd ..
 if [ ! -d "ext" ]; then mkdir -p ext; fi
 if [ ! -d "ext/crosscore" ]; then mkdir -p ext/crosscore; fi
-for xcore in crosscore.hpp crosscore.cpp demo.hpp demo.cpp draw.hpp oglsys.hpp oglsys.cpp oglsys.inc scene.hpp scene.cpp smprig.hpp smprig.cpp draw_ogl.cpp
+
+XCORE_URL="https://raw.githubusercontent.com/schaban/crosscore_dev/main/src"
+XCORE_MAIN="crosscore.hpp crosscore.cpp demo.hpp demo.cpp draw.hpp oglsys.hpp oglsys.cpp oglsys.inc scene.hpp scene.cpp smprig.hpp smprig.cpp draw_ogl.cpp"
+
+if [ "`uname -s`" = "Darwin" ]
+then
+	if [ ! -d "ext/crosscore/mac" ]; then mkdir -p ext/crosscore/mac; fi
+	for xcore in $XCORE_MAIN
+	do
+		curl $XCORE_URL/$xcore -o ext/crosscore/$xcore
+	done
+	for xcore in mac_main.m mac_ifc.h
+	do
+		curl $XCORE_URL/mac/$xcore -o ext/crosscore/mac/$xcore
+	done
+	exit
+fi
+
+for xcore in $XCORE_MAIN
 do
-	wget -O ext/crosscore/$xcore https://raw.githubusercontent.com/schaban/crosscore_dev/main/src/$xcore
+	wget -O ext/crosscore/$xcore $XCORE_URL/$xcore
 done
 
 if [ ! -d "ext/crosscore/ogl" ]; then mkdir -p ext/crosscore/ogl; fi
 for xcore in gpu_defs.h progs.inc shaders.inc
 do
-	wget -O ext/crosscore/ogl/$xcore https://raw.githubusercontent.com/schaban/crosscore_dev/main/src/ogl/$xcore
+	wget -O ext/crosscore/ogl/$xcore $XCORE_URL/ogl/$xcore
 done
 
 for xcore in get_egl_headers.sh get_gles_headers.sh get_ogl_headers.sh
