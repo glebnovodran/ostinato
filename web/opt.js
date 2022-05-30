a0508ff89a9fd37aeb52cb8d8f18c9c0e1aa0956
Glib Novodran <novodran@gmail.com>
2022-05-30 11:44:14 +0200

    opt.js : pseudo shadows mode for web version

--- web/opt.js
+++ web/opt.js
@@ -16,9 +16,13 @@ if (pageParams.get("small") !== null) {
 if (pageParams.get("low") !== null) {
 	Module.arguments.push("-smap:1024");
 	Module.arguments.push("-nostgshadow:1");
+} else if (pageParams.get("pseudoshd") !== null) {
+	Module.arguments.push("-smap:-1");
+	Module.arguments.push("-nostgshadow:1");
 }
 
 if (pageParams.get("vl") !== null) {
 	Module.arguments.push("-vl:1");
 }
+
 
