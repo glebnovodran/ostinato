fbf62d959f98abbba21985ce679024e195eb91d6
Glib Novodran <novodran@gmail.com>
2022-07-23 15:29:38 +0200

    primfx : remove unnecesary bufer updates

--- src/primfx.cpp
+++ src/primfx.cpp
@@ -13,6 +13,9 @@ namespace PrimFX {
 struct Primitives {
 	static const int NUM_VTX = 1000;
 	static const int NUM_IDX = 1500;
+	static const int OFFS_PSHD_VTX = 0;
+	static const int OFFS_PSHD_IDX = 0;
+
 	sxTextureData* mpShdTex;
 	uint32_t mNextVtx = 0;
 	uint32_t mNextIdx = 0;
@@ -23,6 +26,28 @@ struct Primitives {
 		if (pCmnPkg) {
 			mpShdTex = pCmnPkg->find_texture("shd_common_BASE");
 		}
+
+		sxPrimVtx vtx[4];
+		cxVec nrm(0.0f, 1.0f, 0.0f);
+		for (int i = 0; i < 4; ++i) {
+			vtx[i].prm.fill(0.0f);
+			vtx[i].encode_normal(nrm);
+			vtx[i].clr.set(0.05f, 0.05f, 0.05f, 0.75f);
+			vtx[i].tex.fill(0.5f);
+		}
+
+		vtx[0].pos.set(-0.5f, 0.0f, 0.5f, 1.0f);
+		vtx[1].pos.set(0.5f, 0.0f, 0.5f, 1.0f);
+		vtx[2].pos.set(0.5f, 0.0f, -0.5f, 1.0f);
+		vtx[3].pos.set(-0.5f, 0.0f, -0.5f, 1.0f);
+
+		vtx[0].tex.set(0.0f, 0.0f, 0.0f, 0.0f);
+		vtx[1].tex.set(1.0f, 0.0f, 0.0f, 0.0f);
+		vtx[2].tex.set(1.0f, 1.0f, 0.0f, 0.0f);
+		vtx[3].tex.set(0.0f, 1.0f, 0.0f, 0.0f);
+
+		uint16_t idx[] = { 0, 1, 2, 0, 2, 3 };
+		Scene::prim_geom(OFFS_PSHD_VTX, 4, vtx, OFFS_PSHD_IDX, 6, idx);
 	}
 
 	void reset() {
@@ -52,32 +77,9 @@ bool draw_pseudo_shadow(ScnObj* pObj, void* pMem) {
 		}
 
 		cxMtx wm = pObj->calc_skel_world_mtx(iroot);
-		sxPrimVtx vtx[4];
-		cxVec nrm(0.0f, 1.0f, 0.0f);
-		for (int i = 0; i < 4; ++i) {
-			vtx[i].prm.fill(0.0f);
-			vtx[i].encode_normal(nrm);
-			vtx[i].clr.set(0.05f, 0.05f, 0.05f, 0.75f);
-			vtx[i].tex.fill(0.5f);
-		}
-
-		vtx[0].pos.set(-0.5f, 0.0f, 0.5f, 1.0f);
-		vtx[1].pos.set(0.5f, 0.0f, 0.5f, 1.0f);
-		vtx[2].pos.set(0.5f, 0.0f, -0.5f, 1.0f);
-		vtx[3].pos.set(-0.5f, 0.0f, -0.5f, 1.0f);
-
-		vtx[0].tex.set(0.0f, 0.0f, 0.0f, 0.0f);
-		vtx[1].tex.set(1.0f, 0.0f, 0.0f, 0.0f);
-		vtx[2].tex.set(1.0f, 1.0f, 0.0f, 0.0f);
-		vtx[3].tex.set(0.0f, 1.0f, 0.0f, 0.0f);
-
-		uint16_t idx[] = { 0, 1, 2, 0, 2, 3 };
-
 		wm.set_translation(wm.get_translation() + cxVec(0.0f, 0.025f, 0.0f));
-		Scene::prim_geom(s_primWk.mNextVtx, 4, vtx, s_primWk.mNextIdx, 6, idx);
-		Scene::idx_tris_semi_dsided(s_primWk.mNextIdx, 2, &wm, s_primWk.mpShdTex, false);
-		s_primWk.mNextVtx =  s_primWk.mNextVtx <= (Primitives::NUM_VTX - 4) ? s_primWk.mNextVtx + 4 : 0;
-		s_primWk.mNextIdx =  s_primWk.mNextIdx <= (Primitives::NUM_IDX - 6) ? s_primWk.mNextIdx + 6 : 0;
+
+		Scene::idx_tris_semi_dsided(Primitives::OFFS_PSHD_IDX, 2, &wm, s_primWk.mpShdTex, false);
 	}
 	return true;
 }
